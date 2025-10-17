09c2bbbabbf2bf7588fdd574c242d5feae33d638
perceptive2345 <ch.suryachandra2002@gmail.com>
2025-10-17 11:36:20 +0530

    17th oct tasklist pt

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Progresstracker from "./Components/Progresstracker";
 import Taskform from "./Components/Taskform";
 import Tasklist from "./Components/Tasklist";
+import "./Style.css";
 
 function App() {
 const[tasks, setTasks]  = useState([])
@@ -13,13 +14,26 @@ useEffect(() => {
   const addTask = (task) => {
   setTasks([...tasks,task]);
   }
+
+  const updateTask = (updatedTask,index) => {
+  const newTask = [...tasks];
+  newTask[index] = updatedTask;
+  setTasks(newTask);
+  }
+
+   const deleteTask = (index) => {
+   setTasks(tasks.filter((_, i) => i != index))
+   }
+
+
   return(
     <div>
       <h1>Task Nest</h1>
       <p>Our Friendly Task Manager</p>
       <Taskform addTask={addTask}/>
-      <Tasklist />
-      <Progresstracker />
+      <Tasklist tasks = {tasks}
+      updateTask = {updateTask} deleteTask = {deleteTask}/>
+      <Progresstracker tasks={tasks}/>
       <button>Clear</button>
     </div>
   )
