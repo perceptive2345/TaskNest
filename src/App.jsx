36fcbe99fded742bd98efc0167cfb53c75164a34
perceptive2345 <ch.suryachandra2002@gmail.com>
2025-10-18 10:41:43 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
-import { useEffect, useState } from "react";
-import Progresstracker from "./Components/Progresstracker";
 import Taskform from "./Components/Taskform";
-import Tasklist from "./Components/Tasklist";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
 import "./Style.css";
 
-function App() {
-const[tasks, setTasks]  = useState([])
+export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-useEffect(() => {
-  localStorage.setItem("tasks", JSON.stringify(tasks))
-});
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
 
   const addTask = (task) => {
-  setTasks([...tasks,task]);
+    setTasks([...tasks,task]);
   }
 
-  const updateTask = (updatedTask,index) => {
-  const newTask = [...tasks];
-  newTask[index] = updatedTask;
-  setTasks(newTask);
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
   }
 
-   const deleteTask = (index) => {
-   setTasks(tasks.filter((_, i) => i != index))
-   }
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
 
+  const clearTasks = () => {
+    setTasks([]);
+  }
 
   return(
-    <div>
-      <h1>Task Nest</h1>
-      <p>Our Friendly Task Manager</p>
-      <Taskform addTask={addTask}/>
-      <Tasklist tasks = {tasks}
-      updateTask = {updateTask} deleteTask = {deleteTask}/>
-      <Progresstracker tasks={tasks}/>
-      <button>
-       Clear All Tasks
-      </button>
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Our friendly TaskManager</p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
 
+      {tasks.length>0 && 
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
+      
     </div>
   )
 }
-
-export default App;
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,38 +1,40 @@
 import { useState } from "react"
 
 export default function Taskform({addTask}) {
-     const[task,setTask] = useState('');
-     const [priority, setPriority] = useState('medium');
-     const [category, setCategory] = useState('general');
+    const[task, setTask] = useState('');
+    const [priority, setPriority] = useState('medium');
+    const [category, setCategory] = useState('general');
 
-     const handlesubmit = (e) => {
-        e.preventDefault();
+    const handlesubmit = (e) => {
+        e.preventDefault(); //refresh
         addTask({text: task, priority, category, completed: false});
 
         //reset
         setTask('');
         setPriority('medium');
         setCategory('general');
-     }
+    }
+
     return(
-        <form onSubmit={handlesubmit}>
-            <div>
-                <input type="text" placeholder="Enter the task" value={task}
+        <form onSubmit={handlesubmit} className='task-form'>
+            <div id="inp">
+                <input type="text" placeholder="Enter the task" 
+                value={task}
                 onChange={(e) => setTask(e.target.value)}/>
                 <button type="submit">Add Task</button>
-                <h1>{task} {priority} {category}</h1>
+                {/* <h1>{task} {priority} {category}</h1> */}
             </div>
 
-            <div>
+            <div id="btns">
                 <select value={priority} onChange={(e) => setPriority(e.target.value)}>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
-                    <option value="low">Low</option>
+                    <option value="low">low</option>
                 </select>
 
                 <select value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option value="general">General</option>
-                    <option value="work">Work</option>
+                    <option value="work">work</option>
                     <option value="personal">Personal</option>
                 </select>
             </div>
